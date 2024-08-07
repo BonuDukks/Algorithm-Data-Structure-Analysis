814c6fc50652b8ca419649e5e18b304e029fb9b5
BonusDukks <alexander.ung562@gmail.com>
2024-08-07 19:43:47 +0930

    init commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1;
+  int num2;
+
+  cin >> num1 >> num2;
+
+  cout << "Num 1: " << num1 << "\nNum2: " << num2 << endl;
+
+  return 0;
+}
